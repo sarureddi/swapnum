3c46a190ef27b924ec14f254c77588937a2eabb3
saru <sarumathiayyappan@gmail.com>
2019-06-06 12:32:09 +0530

    Create swapnum.py

---
+++ swapnum.py
@@ -0,0 +1,9 @@
+n1=int(input("enter the number1   ")
+n2=int(input("enter the number2   ")
+print("number1 before swapping",n1)
+print("number2 before swapping",n2)
+temp=n1
+n1=n2
+n2=temp
+print("number1 after swapping",n1)
+print("number1 after swapping",n2)
